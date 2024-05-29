fab590cbf620408aa62c0a35b8e79676a6c2203d
ziyadt1995 <67925101+ziyadt1995@users.noreply.github.com>
2024-05-29 08:51:18 +0300

    Update settings.py

--- savings_by_PostCode/settings.py
+++ savings_by_PostCode/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-63ze^kt%2487&%2kctlqrm6haxf_+)sb-$=ki#%7^olnr@)3gl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
